8300e653a5ab41dc9f4ee333bbecbab98858ba0e
Umang Udit <40484466+UmangUdit96@users.noreply.github.com>
2023-06-05 20:11:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ let weather = {
       document.querySelector(".humidity").innerText =
         "Humidity: " + humidity + "%";
       document.querySelector(".wind").innerText =
-        "Wind speed: " + speed + " km/h";
+        "Wind Speed: " + speed + " KM/H";
       document.querySelector(".weather").classList.remove("loading");
       document.body.style.backgroundImage =
         "url('https://source.unsplash.com/1600x900/?" + name + "')";
